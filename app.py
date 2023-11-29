29fbac68c9163d6f2846a62d863f92a15a23db27
Anna Bodily <annalallen6@gmail.com>
2023-11-29 10:45:38 -0700

    made the checkbox actually do something

--- app.py
+++ app.py
@@ -2,18 +2,21 @@ import streamlit as st
 import numpy as np
 
 # Title of the app
-st.title("Simple Streamlit App")
+st.title("Streamlit App with Checkbox")
 
 # Interactive elements
 input_data = st.slider('Select a range of values', 0.0, 100.0, (25.0, 75.0))
 checkbox = st.checkbox('Enable feature')
 
-# Function to generate data based on input
-def generate_data(input_range):
-    return np.linspace(input_range[0], input_range[1], 100)
+# Function to generate data based on input and checkbox
+def generate_data(input_range, enable_feature):
+    if enable_feature:
+        return np.sin(np.linspace(input_range[0], input_range[1], 100))
+    else:
+        return np.linspace(input_range[0], input_range[1], 100)
 
-# Calculate data based on the input
-data = generate_data(input_data)
+# Calculate data based on the input and checkbox
+data = generate_data(input_data, checkbox)
 
 # Display the graph
 st.line_chart(data)
